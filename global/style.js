f18f947b9a7a6e7dc2a377cd347154ff86a248d8
Channaveer Patil <118630837+channaveer24@users.noreply.github.com>
2024-07-05 15:26:32 +0530

    style

---
+++ global/style.js
@@ -0,0 +1,63 @@
+module.exports ={
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width:'100%',
+        height:'100%',
+    },
+    rec: {
+        width: '100%',
+        height: '40%',
+        borderBottomEndRadius: 100,
+        borderBottomStartRadius: 100,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+        elevation:20,
+        
+    },
+    logo:{
+        width:'80%',
+        height:'100%',
+        top:'5%',
+        justifyContent: 'center',
+        alignContent:'center',
+
+      },
+      text :{
+        // fontSize:18,
+        width:'100%',
+        color:'#ECF0F1',
+        // textAlign:'center',
+     
+    //   flexDirection:'column',
+      height:'80%'
+   
+     
+       
+      },
+      btn :{
+        fontSize:16,
+        colour:'#E74C3C',
+        textAlign:'center',
+        alignItems:'flex-end',
+        marginVertical:10,
+        marginHorizontal:60,
+        fontWeight:'700',
+        backgroundColor:'#fff',
+        borderColor:'black',
+        borderEndColor:'black',
+        borderRadius:50,
+        padding:10,
+         elevation :10,   // paddingHorizontal:20,
+  
+      },
+        Lg:{
+        width:'100%',
+        height:'10%',
+        
+        }
+    }
